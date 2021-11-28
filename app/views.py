a8d2051b8ada2eac953883ec7a63687f349c5eb0
john-neg <john_neg@mail.ru>
2021-11-28 14:50:10 +0300

    update

--- app/views.py
+++ app/views.py
@@ -2,14 +2,14 @@ from app import app
 from app.forms import *
 from werkzeug.utils import redirect
 from flask import render_template, request, url_for, session
-
+from flask_session import sessions
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/index", methods=["GET", "POST"])
 def index():
     form = Intro()
     if form.validate_on_submit():
-        session["room"] = request.form.get("room")
+        session['room'] = request.form["room"]
         width = request.form.get("width")
         height = request.form.get("height")
         return redirect(url_for("quest", width=width, height=height))
@@ -18,24 +18,27 @@ def index():
 
 @app.route("/quest/<int:width>x<int:height>", methods=["GET", "POST"])
 def quest(width, height):
-    r = Rooms(5, width + 2, height + 2)  # session['room']
-    form = Quest()
-    message = "Hello"
     if request.method == "POST":
         message = "Move"
+        form = Quest()
+        place = session['place']
         return render_template(
             "start.html",
-            position=r.position,
-            rooms=r.rooms,
-            current_room=r.get_name(),
+            position=place.position,
+            rooms=place.rooms,
+            current_room=place.get_name(),
             form=form,
             message=message,
         )
-    return render_template(
-        "start.html",
-        position=r.position,
-        rooms=r.rooms,
-        current_room=r.get_name(),
-        form=form,
-        message=message,
-    )
+    else:
+        place = Rooms(int(session.pop('room')), width + 2, height + 2)
+        form = Quest()
+        message = "Hello"
+        return render_template(
+            "start.html",
+            position=place.position,
+            rooms=place.rooms,
+            current_room=place.get_name(),
+            form=form,
+            message=message,
+        )
--- config.py
+++ config.py
@@ -2,5 +2,7 @@ import os
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
+
 class FlaskConfig(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'verysupersecretkeystring'
+    SESSION_TYPE = 'filesystem'
